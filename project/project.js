493054c6f11a06aa1a20c31b419629f140edc570
Овсепян Арам <aram.ovsepyan@rtlabs.ru>
2022-11-26 11:37:09 +0300

    pg -> postgresql-client

--- project/project.js
+++ project/project.js
@@ -1,21 +1,18 @@
 const {By, Builder} = require("selenium-webdriver");
-const pg = require('pg')
+const {Connection} = require('postgresql-client');
 require("chromedriver");
 
-const client = new pg.Client({
+const connection = new Connection({
     host: 'postgres-5084.postgresql.a.osc-fr1.scalingo-dbs.com',
     port: 30661,
     database: 'postgres_5084',
     user: 'postgres_5084',
     password: '9NOUJokMT2VfJ8zaxMeQ'
-})
+});
 
 async function main() {
     // Подключиться к базе данных в облаке
-    await client.connect(err => {
-        if (err) console.log('Failed', err)
-        else console.log('Success')
-    })
+    await connection.connect()
 
     // Инициировать драйвер браузера
     let driver = await new Builder().forBrowser("chrome").build();
@@ -42,19 +39,14 @@ async function main() {
         })
 
     // Положить главную новость и список новостей с главной страницы в базу в облаке
-    await client.query(
-        'INSERT INTO news (top_news, top) VALUES (?, ?)',
-        [topNews, top],
-        err => {
-            if (!err) {
-                console.log("Inserted top news")
-            }
-        }
+    await connection.query(
+        'INSERT INTO news (top_news, top) VALUES ($1, $2::json)',
+        { params: [topNews, JSON.stringify(top)]}
     )
 
     // Отключить драйвер и подключение к базе данных
-    await driver.quit();
-    await client.end()
+    await driver.quit()
+    await connection.close()
 }
 
 main().then(() => console.log("Success"))
